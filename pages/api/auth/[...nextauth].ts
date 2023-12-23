57ff906a27fb0e5e50bb24447e1178ed2ccff2f5
Sean Overbeeke <sean@heartbridgemedia.com>
2023-12-23 07:07:43 -0600

    this one

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -8,6 +8,14 @@ import { compare } from "bcrypt";
 import prismadb from "../../../lib/prismadb";
 export default NextAuth({
   providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID || "",
+      clientSecret: process.env.GITHUB_SECRET || "",
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
     Credentials({
       id: "credentials",
       name: "Credentials",
