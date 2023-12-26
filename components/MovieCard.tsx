55f1e0aa4929ad99ce0d7982a89ba7352de79f35
Basit Ali <alib4111@gmail.com>
2023-12-27 01:45:57 +0500

    styling issue fixed

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,22 +1,23 @@
-import { BsFillPlayFill } from "react-icons/bs";
-import { BiChevronDown } from "react-icons/bi";
-import { useRouter } from "next/router";
-import FavoriteButton from "./FavoriteButton";
-import PlayButton from "./PlayButton";
-import useInfoModal from "../hooks/useInfoModal";
+import { BsFillPlayFill } from 'react-icons/bs';
+import { BiChevronDown } from 'react-icons/bi';
+import { useRouter } from 'next/router';
+import FavoriteButton from './FavoriteButton';
+import PlayButton from './PlayButton';
+import useInfoModal from '../hooks/useInfoModal';
 interface MovieCardProps {
-  data: Record<string, any>;
+	data: Record<string, any>;
 }
 const CURRENT_YEAR = new Date().getFullYear();
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
-  const router = useRouter();
-  const { openModal } = useInfoModal();
-  const { thumbnailUrl, duration, genre, releasedDate, createdAt, id } = data;
-  const createdAtYear = new Date(createdAt).getFullYear();
-  return (
-    <div className="group bg-zinc-900 col-span-1 relative h-[12vw]">
-      <img
-        className="
+	const router = useRouter();
+	const { openModal } = useInfoModal();
+	const { thumbnailUrl, duration, genre, releasedDate, createdAt, id } = data;
+	const createdAtYear = new Date(createdAt).getFullYear();
+	return (
+		<div className='group bg-zinc-900 col-span-1 relative'>
+			<div className='aspect-w-16 aspect-h-9'>
+				<img
+					className='
             cursor-pointer
             object-cover
             transition
@@ -26,14 +27,14 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
             group-hover:opacity-90
             sm:group-hover:opacity-0
             delay-300
-            w-full
-            h-[12vw]
-            "
-        src={thumbnailUrl}
-        alt="Thumbnail"
-      />
-      <div
-        className="
+            '
+					src={thumbnailUrl}
+					alt='Thumbnail'
+				/>
+			</div>
+
+			<div
+				className='
         opacity-0
         absolute
         top-0
@@ -49,10 +50,10 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
         group-hover:-translate-y-[6vw]
         group-hover:translate-x-[2vw]
         group-hover:opacity-100
-"
-      >
-        <img
-          className="
+'
+			>
+				<img
+					className='
             cursor-pointer
             object-cover
             transition
@@ -61,12 +62,12 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
             rounded-t-md
             w-full
             h-[12vw]
-            "
-          src={thumbnailUrl}
-          alt="Thumbnail"
-        />
-        <div
-          className="
+            '
+					src={thumbnailUrl}
+					alt='Thumbnail'
+				/>
+				<div
+					className='
       z-10
       bg-zinc-800
       p-2
@@ -77,14 +78,14 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
       shadow-md
       rounded-b-md
 
-    "
-        >
-          <div className="flex flex-row items-center gap-3">
-            <div
-              onClick={() => {
-                router.push(`/watch/${id}`);
-              }}
-              className="
+    '
+				>
+					<div className='flex flex-row items-center gap-3'>
+						<div
+							onClick={() => {
+								router.push(`/watch/${id}`);
+							}}
+							className='
               cursor-pointer
               w-6
               h-6
@@ -98,13 +99,13 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
               transition
               hover:bg-neutral-300
 
-            "
-            >
-              <BsFillPlayFill size={25} />
-            </div>
-            <FavoriteButton movieId={id} />
-            <div
-              className="
+            '
+						>
+							<BsFillPlayFill size={25} />
+						</div>
+						<FavoriteButton movieId={id} />
+						<div
+							className='
             cursor-pointer
             ml-auto
             group/item
@@ -119,27 +120,28 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
             justify-center 
             items-center 
             transition 
-            hover:border-neutral-300 "
-            >
-              <BiChevronDown 
-              onClick={() => openModal(id)}
-              size={25}
-              className="text-white group-hover/item:text-neutral-300"/>
-            </div>
-          </div>
-          <p className="text-green-400 font-semibold mt-4">
-            {createdAtYear === CURRENT_YEAR ? "New" : ""}{" "}
-            <span className="text-white">{releasedDate}</span>
-          </p>
-          <div className="flex flex-row mt-4 gap-2 items-center">
-            <p className="text-white text-[10px] lg:text-sm">{duration}</p>
-          </div>
-          <div className="flex flex-row mt-4 gap-2 items-center">
-            <p className="text-white text-[10px] lg:text-sm">{genre}</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+            hover:border-neutral-300 '
+						>
+							<BiChevronDown
+								onClick={() => openModal(id)}
+								size={25}
+								className='text-white group-hover/item:text-neutral-300'
+							/>
+						</div>
+					</div>
+					<p className='text-green-400 font-semibold mt-4'>
+						{createdAtYear === CURRENT_YEAR ? 'New' : ''}{' '}
+						<span className='text-white'>{releasedDate}</span>
+					</p>
+					<div className='flex flex-row mt-4 gap-2 items-center'>
+						<p className='text-white text-[10px] lg:text-sm'>{duration}</p>
+					</div>
+					<div className='flex flex-row mt-4 gap-2 items-center'>
+						<p className='text-white text-[10px] lg:text-sm'>{genre}</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 export default MovieCard;
